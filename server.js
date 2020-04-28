98f1043ebd19bd3df2ffe04d7bdade7b7de0b5d4
Chris Nguyen <tridnguyen97@gmail.com>
2020-04-28 20:45:49 +0930

    from public to build

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname,'public');
+const buildPath = path.join(__dirname,'build');
 const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
+app.use(express.static(buildPath));
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+   res.sendFile(path.join(buildPath, 'index.html'));
 });
 app.listen(port, () => {
    console.log('Server is up!');
